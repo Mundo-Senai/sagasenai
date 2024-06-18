e37f085829c029e735162ce4d65175824b166852
Gabriel Henrique <106666571+Purolinho@users.noreply.github.com>
2024-06-18 09:36:55 -0300

    Create conexao.php

---
+++ src/php/config/conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+    $host = "localhost";
+    $usuario = 'root';
+    $senha = "";
+    $nome_db = "saga_senai";
+
+    $bd_conexao = new mysqli($host, $usuario, $senha, $nome_db);
+
+    if($bd_conexao->connect_errno) {
+        die("CONNECTION <br>". $bd_conexao->connect_errno);
+    }
