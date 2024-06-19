c6c91d753d0629f271fd0bd228c525a44f8d4352
Joao Marcos Santana <joaosantana@xgrow.com>
2024-06-19 19:54:04 -0300

    Registro feito

--- src/php/config/conexao.php
+++ src/php/config/conexao.php
@@ -1,12 +1,12 @@
 <?php
 
-    $host = "localhost";
+    $host = 'localhost';
     $usuario = 'root';
-    $senha = "usbw";
-    $nome_db = "saga_senai";
+    $senha = '';
+    $nome_db = 'saga_senai';
 
     $bd_conexao = new mysqli($host, $usuario, $senha, $nome_db);
 
     if($bd_conexao->connect_errno) {
-        die("CONNECTION <br>". $bd_conexao->connect_errno);
+        die('CONNECTION <br>' . $bd_conexao->connect_errno);
     }
--- src/php/controle/registro.php
+++ src/php/controle/registro.php
@@ -6,12 +6,9 @@
     $senha = password_hash(mysqli_real_escape_string($bd_conexao, $_POST['senha']), PASSWORD_DEFAULT, [12]);
     $papel = mysqli_real_escape_string($bd_conexao, $_POST['papel']);
 
-
-
-    function Registro ($bd_conexao, $email, $nome, $senha, $papel) {
-        $verificarEmail  = mysqli_query($bd_conexao, "INSERT INTO usuarios (email, nome, senha, papel) VALUES ('$email', '$nome', '$senha', '$papel')");
-    }
-    echo Registro($bd_conexao, 'teste@gmail', 'teste', 'teste', 1);
+    $preapre = $bd_conexao->prepare('INSERT INTO usuarios(email, nome, senha, papel) VALUES (?, ?, ?, ?)');
+    $preapre->bind_param('ssss', $nome, $data_inicio, $data_fim, $professor);
+    $preapre->execute();
 
 
     
