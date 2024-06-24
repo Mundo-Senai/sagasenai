7629d13596b5b47245c7f57c1297ebce40fd2b77
Gabriel Henrique <106666571+Purolinho@users.noreply.github.com>
2024-06-24 09:40:04 -0300

    criado criar_curso

--- src/php/controle/criar_curso.php
+++ src/php/controle/criar_curso.php
@@ -2,14 +2,20 @@
     
     include "../../php/config/conexao.php";
 
-    $nome = mysqli_real_escape_string($bd_conexao, $_POST['codigo']);
+    $nome = mysqli_real_escape_string($bd_conexao, $_POST['nome']);
     $data_inicio = mysqli_real_escape_string($bd_conexao, $_POST['data_inicio']);
     $data_fim = mysqli_real_escape_string($bd_conexao, $_POST['data_fim']);
     $professor = mysqli_real_escape_string($bd_conexao, $_POST['professor']);
-    
-    $preapre = $bd_conexao->prepare('INSERT INTO cursos(nome, data_inicio, data_fim, professor_id) VALUES (?, ?, ?, ?)');
-    $preapre->bind_param('ssss', $nome, $data_inicio, $data_fim, $professor);
-    $preapre->execute();
 
-    $preapre->close();
-    $bd_conexao->close();
+    if(isset($nome) && isset($data_inicio) && isset($data_fim) && isset($professor)) {
+
+        $preapre = $bd_conexao->prepare('INSERT INTO cursos(nome, data_inicio, data_fim, professor_id) VALUES (?, ?, ?, ?)');
+        $preapre->bind_param('sssi', $nome, $data_inicio, $data_fim, $professor);
+        $preapre->execute();
+    
+        $preapre->close();
+        $bd_conexao->close();
+
+    }
+
+    header("Location: ../html/index.html");
