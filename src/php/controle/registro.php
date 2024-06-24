df4acfcc9f898645baf1f2658b3dd458b3614454
Gabriel Henrique <106666571+Purolinho@users.noreply.github.com>
2024-06-24 09:57:07 -0300

    registro alterado

--- src/php/controle/registro.php
+++ src/php/controle/registro.php
@@ -5,10 +5,14 @@
     $nome = mysqli_real_escape_string($bd_conexao, $_POST['nome']);
     $senha = password_hash(mysqli_real_escape_string($bd_conexao, $_POST['senha']), PASSWORD_DEFAULT, [12]);
     $papel = mysqli_real_escape_string($bd_conexao, $_POST['papel']);
+    $curso = mysqli_real_escape_string($bd_conexao, $_POST['curso']);
 
-    $preapre = $bd_conexao->prepare('INSERT INTO usuarios(email, nome, senha, papel) VALUES (?, ?, ?, ?)');
-    $preapre->bind_param('ssss', $nome, $data_inicio, $data_fim, $professor);
+
+    $preapre = $bd_conexao->prepare('INSERT INTO usuarios(email, nome, senha, papel, cursos_id) VALUES (?, ?, ?, ?, ?)');
+    $preapre->bind_param('sssis', $email, $nome, $senha, $papel, $curso);
     $preapre->execute();
 
+    $preapre->close();
+    $bd_conexao->close();
 
     
